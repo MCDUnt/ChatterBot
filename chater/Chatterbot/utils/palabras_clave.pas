1acd8d28bd467852ed1d721c82e60f9e27592b7f
Mario Caballero <mario.caballero.948@gmail.com>
2025-01-31 14:21:15 -0300

    feat: palabras clave para buscar en la entrada

---
+++ chater/Chatterbot/utils/palabras_clave.pas
@@ -0,0 +1,26 @@
+
+Unit palabras_clave;
+
+Interface
+
+Const 
+  genero: array[1..23] Of string = ('drama', 'comedia', 'accion', 'aventura',
+                                    'sci-fi', 'misterio', 'crimen', 'thriller',
+                                    'fantasia', 'romance', 'historia',
+                                    'animacion', 'horror', 'biografia',
+                                    'musical', 'deporte', 'guerra', 'familiar',
+                                    'documental', 'western', 'musica',
+                                    'suspenso', 'filosofico');
+
+  epoca: array[1..11] Of string = ('epoca', 'año', 'anio', 'años', 'anios',
+                                   '60', '70', '80', '90', 'clasicos',
+                                   'recientes');
+
+  actor: array[1..2] Of string = ('me gusta', 'soy fan de');
+
+  director: array[1..2] Of string = ('dirigidas por', 'dirigida por');
+
+
+Implementation
+
+End.
