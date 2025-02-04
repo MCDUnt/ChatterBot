1c5949dc84bb4745f08bc0b393193ef36918ffcf
mario <mario.caballero.948@gmail.com>
2025-02-03 21:44:10 -0300

    refactor: rutas de archivos

--- chater/Chatterbot/entidad.pas
+++ chater/Chatterbot/entidad.pas
@@ -4,19 +4,7 @@ Unit entidad;
 Interface
 
 Const 
-  RUTA = '../archivos/peliculas.DAT';
-
-Type 
-
-  T_DATO = Record
-    Titulo: string[20];
-    Genero: string[20];
-    Anualidad: string[20];
-    Productora: string[20];
-    Actor_principal: string[20];
-  End;
-
-  T_ARCHIVO = FILE Of T_DATO;
+  RUTA = '../archivos/peliculas_reales.txt';
 
 Implementation
 
--- chater/Chatterbot/iniciar_archivo.pas
+++ chater/Chatterbot/iniciar_archivo.pas
@@ -5,15 +5,12 @@ Interface
 
 Uses entidad;
 
-Procedure iniciarArchivo();
-Procedure cerrarArchivo(Var arch: T_ARCHIVO);
+Procedure iniciarArchivo(Var arch: TextFile);
+Procedure cerrarArchivo(Var arch: TextFile);
 
 Implementation
 
-Procedure iniciarArchivo();
-
-Var 
-  arch: T_ARCHIVO;
+Procedure iniciarArchivo(Var arch: TextFile);
 Begin
   Assign(arch, RUTA);
   //Para chequear se puede utilizar las directivas al compilador:
@@ -25,7 +22,7 @@ Begin
   If IOResult<>0 Then Rewrite(arch); {si no existe lo crea}
 End;
 
-Procedure cerrarArchivo(Var arch: T_ARCHIVO);
+Procedure cerrarArchivo(Var arch: TextFile);
 Begin
   Close(arch);
 End;
