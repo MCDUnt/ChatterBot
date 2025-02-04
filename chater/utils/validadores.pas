d24266a110b45fc4697c14734ed77aadc480edfe
mario <mario.caballero.948@gmail.com>
2025-02-03 21:43:49 -0300

    feat: se agrega epoca y genero

--- chater/utils/validadores.pas
+++ chater/utils/validadores.pas
@@ -8,6 +8,12 @@ Interface
 Uses sysutils, palabras_claves;
 
 Function ContienSalir(cadena: String): Boolean;
+Function ContieneGenero(cadena: String): Boolean;
+Function ContieneDirector(cadena: String): Boolean;
+Function ContieneActor(cadena: String): Boolean;
+Function ContieneEpoca(cadena: String): Boolean;
+Procedure BuscarGenero(cadena: String; Var gen: String);
+Procedure BuscarEpoca(cadena: String; Var ep: String);
 
 Implementation
 
@@ -17,11 +23,120 @@ Var
   i: Integer;
 Begin
   ContienSalir := false;
+  i := 1;
   While (i <= 13) And (Not ContienSalir) Do
     Begin
       If Pos(despedida[i], cadena) > 0 Then
         Begin
           ContienSalir := True;
+        End;
+      Inc(i);
+    End;
+End;
+
+Function ContieneGenero(cadena: String): Boolean;
+
+Var 
+  i: Integer;
+Begin
+  ContieneGenero := false;
+  i := 1;
+  While (i <= 23) And (Not ContieneGenero) Do
+    Begin
+      If Pos(genero[i], cadena) > 0 Then
+        Begin
+          ContieneGenero := True;
+        End;
+      Inc(i);
+    End;
+End;
+
+Procedure BuscarGenero(cadena: String; Var gen: String);
+
+Var 
+  i: Integer;
+  enc: Boolean;
+Begin
+  gen := '';
+  i := 1;
+  enc := false;
+  While (i <= 23) And (Not enc) Do
+    Begin
+      If Pos(genero[i], cadena) > 0 Then
+        Begin
+          gen := genero[i];
+          enc := True;
+        End;
+      Inc(i);
+    End;
+End;
+
+Function ContieneDirector(cadena: String): Boolean;
+
+Var 
+  i: Integer;
+Begin
+  ContieneDirector := false;
+  i := 1;
+  While (i <= 2) And (Not ContieneDirector) Do
+    Begin
+      If Pos(director[i], cadena) > 0 Then
+        Begin
+          ContieneDirector := True;
+        End;
+      Inc(i);
+    End;
+End;
+
+Function ContieneActor(cadena: String): Boolean;
+
+Var 
+  i: Integer;
+Begin
+  ContieneActor := false;
+  i := 1;
+  While (i <= 2) And (Not ContieneActor) Do
+    Begin
+      If Pos(actor[i], cadena) > 0 Then
+        Begin
+          ContieneActor := True;
+        End;
+      Inc(i);
+    End;
+End;
+
+Function ContieneEpoca(cadena: String): Boolean;
+
+Var 
+  i: Integer;
+Begin
+  ContieneEpoca := false;
+  i := 1;
+  While (i <= 11) And (Not ContieneEpoca) Do
+    Begin
+      If Pos(epoca[i], cadena) > 0 Then
+        Begin
+          ContieneEpoca := True;
+        End;
+      Inc(i);
+    End;
+End;
+
+Procedure BuscarEpoca(cadena: String; Var ep: String);
+
+Var 
+  i: Integer;
+  enc: Boolean;
+Begin
+  ep := '';
+  i := 1;
+  enc := false;
+  While (i <= 23) And (Not enc) Do
+    Begin
+      If Pos(epoca[i], cadena) > 0 Then
+        Begin
+          ep := epoca[i];
+          enc := True;
         End;
       Inc(i);
     End;
