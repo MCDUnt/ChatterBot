749075a18e975b50a8e99902bd9170985b9578cf
mario <mario.caballero.948@gmail.com>
2025-02-03 21:44:39 -0300

    feat: se agregan respuestas

--- chater/utils/unit2_chat.pas
+++ chater/utils/unit2_chat.pas
@@ -9,6 +9,10 @@ Uses palabras_claves;
 
 Function SaludosInicio(): string;
 Function SaludosDespedida(): string;
+Function IngresoErroneo(): string;
+Function RespPositiva(): string;
+Function RespNegativa(): string;
+Function RespNegativaFinal(): string;
 
 Implementation
 
@@ -28,6 +32,42 @@ Begin
   randomize;
   i := random(10);
   WriteLn(saludosAdios[i]);
+End;
+
+Function IngresoErroneo(): string;
+
+Var i: integer;
+Begin
+  randomize;
+  i := random(10);
+  WriteLn(resp_ingreso_neg[i]);
+End;
+
+Function RespPositiva(): string;
+
+Var i: integer;
+Begin
+  randomize;
+  i := random(10);
+  WriteLn(resp_positivas[i]);
+End;
+
+Function RespNegativa(): string;
+
+Var i: integer;
+Begin
+  randomize;
+  i := random(10);
+  WriteLn(resp_negativas[i]);
+End;
+
+Function RespNegativaFinal(): string;
+
+Var i: integer;
+Begin
+  randomize;
+  i := random(10);
+  WriteLn(resp_neg_final[i]);
 End;
 
 End.
