ce43bead1d560490bcfd8351c5e8822f307911da
mario <mario.caballero.948@gmail.com>
2025-02-03 21:45:01 -0300

    feat: busqueda de pelicula y resultados

--- chater/Chatterbot/menu.pas
+++ chater/Chatterbot/menu.pas
@@ -6,7 +6,7 @@ Interface
 
 {$unitpath ../utils/}
 
-Uses crt, validadores, unit2_chat;
+Uses crt, validadores, unit2_chat, unit1_archivo, iniciar_archivo;
 
 Procedure menu_principal;
 
@@ -15,28 +15,53 @@ Implementation
 Procedure menu_principal;
 
 Var 
-  respuesta: STRING;
-
+  f: TextFile;
+  respuesta, peli: STRING;
+  enc: Boolean;
 Begin
-  // iniciarArchivo();
+  iniciarArchivo(f);
   clrscr;
   WriteLn;
   textcolor(green);
   SaludosInicio();
-  WriteLn;
-  textcolor(white);
-  ReadLn(respuesta);
-  WriteLn;
-  If (Length(respuesta) > 0) Then
-    Begin
-      If (Not ContienSalir(lowercase(respuesta))) Then
-        writeln('No esta la palabra salir')
-      Else
-        Begin
-          textcolor(green);
-          SaludosDespedida();
-        End;
-    End;
+  Repeat
+    textcolor(white);
+    WriteLn;
+    readln(respuesta);
+    WriteLn;
+    If (Length(respuesta) > 0) Then
+      Begin
+        If (length(respuesta) < 4) Then
+          Begin
+            textcolor(green);
+            IngresoErroneo();
+          End
+        Else
+          Begin
+            If (Not ContienSalir(lowercase(respuesta))) Then
+              Begin
+                textcolor(green);
+                obtenerPalabrasClave(f, respuesta, peli, enc);
+                If (enc) Then
+                  Begin
+                    RespPositiva();
+                    textcolor(blue);
+                    WriteLn(peli);
+                  End
+                Else
+                  Begin
+                    RespNegativa();
+                    // RespNegativaFinal();
+                    // textcolor(yellow);
+                    // writeln(peli);
+                  End;
+              End;
+          End;
+      End;
+  Until ContienSalir(lowercase(respuesta));
+  textcolor(green);
+  SaludosDespedida();
+  cerrarArchivo(f);
 End;
 
 End.
--- chater/utils/unit1_archivo.pas
+++ chater/utils/unit1_archivo.pas
@@ -3,10 +3,10 @@ Unit unit1_archivo;
 
 Interface
 
-Uses SysUtils;
+Uses SysUtils, validadores;
 
-Procedure ObtenerDatos(linea: String; Var titulo, autor, genero, actor, anio:
-                       String);
+Procedure obtenerPalabrasClave(Var f: TextFile; cadena: String; Var pelicula:
+                               String; Var enc: Boolean);
 
 Implementation
 
@@ -42,18 +42,148 @@ Begin
   anio := partes[5];
 End;
 
-Procedure BuscarEnArchivo(Var f: TextFile; claves: String);
+Procedure BuscarEnArchivo(Var f: TextFile; cadena,clave: String; Var resp:
+                          String);
 
 Var 
   linea, titulo, autor, genero, actor, anio: string;
   enc: Boolean;
 Begin
+  enc := false;
+  reset(f);
   While (Not Eof(f)) And (Not enc) Do
     Begin
       ReadLn(f, linea);
       ObtenerDatos(linea, titulo, autor, genero, actor, anio);
-      If titulo = clave Then
-        enc := true;
+      Case clave Of 
+        'direc':
+                 Begin
+                   If pos(autor, cadena) > 0 Then
+                     Begin
+                       resp := autor;
+                       enc := true;
+                     End;
+                 End;
+        'actor':
+                 Begin
+                   If pos(actor, cadena) > 0 Then
+                     Begin
+                       resp := actor;
+                       enc := true;
+                     End;
+                 End;
+        'anio':
+                Begin
+                  If pos(anio, cadena) > 0 Then
+                    Begin
+                      resp := anio;
+                      enc := true;
+                    End;
+                End;
+      End;
     End;
+End;
+
+Procedure buscarPelicula(Var f: TextFile; gen, act, an, direc: String; Var
+                         pel: String; Var enc: Boolean);
+
+Var flag: Boolean;
+  linea, titulo, autor, genero, actor, anio: string;
+Begin
+  enc := false;
+  reset(f);
+  While Not (Eof(f)) And (Not enc) Do
+    Begin
+      ReadLn(f, linea);
+      ObtenerDatos(linea, titulo, autor, genero, actor, anio);
+      If (length(gen) > 0) And (gen = genero) Then
+        Begin
+          enc := True;
+          pel := linea;
+        End
+      Else If (Length(act) > 0) And (autor = act) Then
+             Begin
+               enc := True;
+               pel := linea;
+             End
+      Else If (length(direc) > 0) And (direc = autor) Then
+             Begin
+               enc := True;
+               pel := linea;
+             End
+      Else If (Length(an) > 0) And (an = anio) Then
+             Begin
+               enc := True;
+               pel := linea;
+             End
+    End;
+  // writeln('aca 4');
+  // If (Not enc) And (Length(act) > 0) Then
+  //   Begin
+  //     writeln('aca 5');
+  //     // reset(f);
+  //     flag := false;
+  //     While Not (eof) And (Not flag) Do
+  //       Begin
+  //         ReadLn(f, linea);
+  //         ObtenerDatos(linea, titulo, autor, genero, actor, anio);
+  //         If (actor = act) Then
+  //           Begin
+  //             flag := true;
+  //             pel := linea;
+  //           End;
+  //       End;
+  //   End
+  // Else If (Not enc) And (Length(gen) > 0) Then
+  //        Begin
+  //          writeln('aca 5');
+  //          //  reset(f);
+  //          flag := false;
+  //          While Not (eof) And (Not flag) Do
+  //            Begin
+  //              ReadLn(f, linea);
+  //              ObtenerDatos(linea, titulo, autor, genero, actor, anio);
+  //              If (gen = genero) Then
+  //                Begin
+  //                  flag := true;
+  //                  pel := linea;
+  //                End;
+  //            End;
+  //        End
+  // Else If (Not enc) And (Length(an) > 0) Then
+  //        Begin
+  //          writeln('aca 6');
+  //          //  reset(f);
+  //          flag := false;
+  //          While Not (eof) And (Not flag) Do
+  //            Begin
+  //              ReadLn(f, linea);
+  //              ObtenerDatos(linea, titulo, autor, genero, actor, anio);
+  //              If (an = anio) Then
+  //                Begin
+  //                  flag := true;
+  //                  pel := linea;
+  //                End;
+  //            End;
+  //        End;
+
+End;
+
+Procedure obtenerPalabrasClave(Var f: TextFile; cadena: String; Var pelicula:
+                               String; Var enc: Boolean);
+
+Var 
+  gen, actor, direc, anio: string;
+Begin
+  If ContieneGenero(cadena) Then
+    BuscarGenero(cadena, gen);
+  If ContieneDirector(cadena) Then
+    BuscarEnArchivo(f, cadena, 'direc', direc);
+  If ContieneEpoca(cadena) Then
+    BuscarEnArchivo(f, cadena, 'anio', anio);
+  BuscarEnArchivo(f, cadena, 'actor', actor);
+
+  buscarPelicula(f, gen, actor, anio, direc, pelicula, enc);
+
 End;
 End.
